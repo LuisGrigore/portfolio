d5d925472581463b8e7cbda648a86bbd588e3745
LuisGrigore <luiscristiangrigore11@gmail.com>
2025-10-13 00:10:20 +0200

    Theme toggle added.

---
+++ src/components/theme_toggle/UseThemeToggle.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export const useThemeToggle = (): readonly [boolean, () => void] => {
+  const [isDarkMode, setDarkMode] = useState<boolean>(() => {
+    const theme = localStorage.getItem("theme");
+    return theme === "dark";
+  });
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", isDarkMode);
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
+
+  const toggleTheme = (): void => setDarkMode((prev) => !prev);
+
+  return [isDarkMode, toggleTheme] as const;
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,13 @@
+import { ThemeToggle } from "../components/theme_toggle/ThemeToggle";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/*Theme toggle*/}
-
+	  <div className="fixed max-sm:hidden top-5 right-5 z-50 p-2 ">
+		<ThemeToggle />
+	  </div>
+      
       {/*Background*/}
 
       {/*Nav Bar*/}
